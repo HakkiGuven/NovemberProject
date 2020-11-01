221ec6f5565b7915aa16861f9c14881a9c705a60
HakkiGuven <kinoss1255@gmail.com>
2020-11-01 11:45:38 -0500

    added flash class to the project

--- src/test/java/Batman.java
+++ src/test/java/Batman.java
@@ -2,6 +2,10 @@ public class Batman {
 
     public static void main(String[] args) {
 
+        System.out.println("Joker I will catch you");
+        System.out.println("Batman has Bat mobile");
+        System.out.println("Batman has bat bike");
+
 
     }
 }
